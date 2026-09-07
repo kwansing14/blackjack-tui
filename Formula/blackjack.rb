40322827e146fc08757e9480af5e6aa627b0dec1
Kwansing_Ng <kwansing_ng@singaporeair.com.sg>
2026-09-07 11:00:32 +0800

    Formula: v0.1.2

--- Formula/blackjack.rb
+++ Formula/blackjack.rb
@@ -1,15 +1,15 @@
 class Blackjack < Formula
   desc "Two-player blackjack in the terminal, over the internet"
   homepage "https://github.com/kwansing14/blackjack-tui"
-  version "0.1.1"
+  version "0.1.2"
 
   on_arm do
-    url "https://github.com/kwansing14/blackjack-tui/releases/download/v0.1.1/blackjack-aarch64-apple-darwin.tar.gz"
-    sha256 "6c1590c672caa9ef0e30b3ac462636667e94bb965b6fddd35bcbe033cef2344f"
+    url "https://github.com/kwansing14/blackjack-tui/releases/download/v0.1.2/blackjack-aarch64-apple-darwin.tar.gz"
+    sha256 "77f43f0018d17735ff5effc4a3f57f70e9fe2a2a376ff2e12bc3f746868383e9"
   end
   on_intel do
-    url "https://github.com/kwansing14/blackjack-tui/releases/download/v0.1.1/blackjack-x86_64-apple-darwin.tar.gz"
-    sha256 "bdbfad428dc30878b025d09409b9e7d3e6976ec25f38724f1bebf4d368487b75"
+    url "https://github.com/kwansing14/blackjack-tui/releases/download/v0.1.2/blackjack-x86_64-apple-darwin.tar.gz"
+    sha256 "547691e882ee74f67fecd0648cc8b07d68ce4efc98c67761d0934154bc49c168"
   end
 
   def install
